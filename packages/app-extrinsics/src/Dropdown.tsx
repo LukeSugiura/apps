4fe491cc5d70b70401432d77b78b9a8487b64cb6
Luke Sugiura <jun@dappface.com>
2019-11-28 14:50:01 +0100

    [WIP] implement AddressField

---
+++ packages/app-extrinsics/src/Dropdown.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button/Button';
+import SUIDropdown from 'semantic-ui-react/dist/commonjs/modules/Dropdown/Dropdown';
+import styled from 'styled-components'
+import { Labelled } from '@polkadot/react-components';
+
+type Props = React.ComponentProps<typeof SUIDropdown> & Pick<React.ComponentProps<typeof Labelled>, 'className' | 'help' | 'label' | 'labelExtra' | 'style' | 'withEllipsis' | 'withLabel'>
+
+// [TODO] use util on @polkadot/react-compoenntns
+function classes (...classNames: (boolean | null | string | undefined)[]): string {
+  return classNames
+    .filter((className): boolean => !!className)
+    .join(' ');
+}
+
+function Dropdown ({
+  className,
+  help,
+  label,
+  labelExtra,
+  style,
+  withEllipsis,
+  withLabel,
+  ...dropdownProps
+}: Props): React.ReactElement {
+  return dropdownProps.button ? (
+    <Button.Group primary>
+      <SUIDropdown {...dropdownProps} />
+    </Button.Group>
+  ) : (
+    <Labelled
+      className={classes('ui--Dropdown', className)}
+      help={help}
+      label={label}
+      labelExtra={labelExtra}
+      style={style}
+      withEllipsis={withEllipsis}
+      withLabel={withLabel}
+    >
+      <SUIDropdown {...dropdownProps} />
+    </Labelled>
+    );
+  ;
+}
+
+export default styled(Dropdown)`
+  .ui--Dropdown-item {
+    position: relative;
+    white-space: nowrap;
+
+    .ui--Dropdown-icon,
+    .ui--Dropdown-name {
+      display: inline-block;
+    }
+
+    .ui--Dropdown-icon {
+      height: 32px;
+      left: 0;
+      position: absolute;
+      top: -9px;
+      width: 32px;
+    }
+
+    .ui--Dropdown-name {
+      margin-left: 3rem;
+    }
+  }
+
+  .ui.selection.dropdown {
+    > .text > .ui--Dropdown-item {
+      .ui--Dropdown-icon {
+        left: -2.6rem;
+        top: -1.15rem;
+        opacity: 1;
+      }
+
+      .ui--Dropdown-name {
+        margin-left: 0;
+      }
+    }
+  }
+`;
--- packages/app-extrinsics/src/Selection.tsx
+++ packages/app-extrinsics/src/Selection.tsx
@@ -8,118 +8,164 @@ import { QueueTxExtrinsicAdd } from '@polkadot/react-components/Status/types';
 import { ApiProps } from '@polkadot/react-api/types';
 import { SubmittableExtrinsic } from '@polkadot/api/promise/types';
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Button, Extrinsic, InputAddress, TxButton, TxComponent } from '@polkadot/react-components';
 import { withApi, withMulti } from '@polkadot/react-api';
 import { BalanceFree } from '@polkadot/react-query';
+import { Formik, useField } from 'formik';
+import styled from 'styled-components'
 
-import translate from './translate';
+import Dropdown from './Dropdown';
+import { useTranslation } from './translate';
 
-interface Props extends ApiProps, I18nProps {
-  queueExtrinsic: QueueTxExtrinsicAdd;
+function AddressField (): React.ReactElement {
+  const { t } = useTranslation()
+  const [field, meta] = useField('accountId')
+
+  const handleChange = (_: any, { value }: { value: any}) => {
+    field.onChange('accountId')(value)
+  }
+
+  return (
+    <Dropdown
+      className={'ui--InputAddress'}
+      error={meta.touched && typeof meta.error === 'undefined'}
+      fluid
+      label={t('using the selected account')}
+      options={[{key: 'a', text: 'a', value: 'a'}, {key: 'b', text: 'b', value: 'b'}]}
+      search
+      selection
+      onChange={handleChange}
+      value={field.value}
+    />
+  );
 }
 
-interface State {
-  isValid: boolean;
-  isValidUnsigned: boolean;
-  method: Call | null;
-  accountId?: string | null;
+export default function (): React.ReactElement {
+  const initialValues = useMemo(() => ({
+    accountId: ''
+  }), []);
+
+  return (
+    <div className='extrinsics--Selection'>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={() => {}}
+      >
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <AddressField />
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
 }
+// interface Props extends ApiProps, I18nProps {
+//   queueExtrinsic: QueueTxExtrinsicAdd;
+// }
 
-class Selection extends TxComponent<Props, State> {
-  public state: State = {
-    isValid: false,
-    isValidUnsigned: false,
-    method: null
-  };
+// interface State {
+//   isValid: boolean;
+//   isValidUnsigned: boolean;
+//   method: Call | null;
+//   accountId?: string | null;
+// }
 
-  public render (): React.ReactNode {
-    const { apiDefaultTxSudo, t } = this.props;
-    const { isValid, isValidUnsigned, accountId } = this.state;
-    const extrinsic = this.getExtrinsic() || apiDefaultTxSudo;
+// class Selection extends TxComponent<Props, State> {
+//   public state: State = {
+//     isValid: false,
+//     isValidUnsigned: false,
+//     method: null
+//   };
 
-    return (
-      <div className='extrinsics--Selection'>
-        <InputAddress
-          label={t('using the selected account')}
-          labelExtra={<BalanceFree label={<label>{t('free balance')}</label>} params={accountId} />}
-          onChange={this.onChangeSender}
-          type='account'
-        />
-        <Extrinsic
-          defaultValue={apiDefaultTxSudo}
-          label={t('submit the following extrinsic')}
-          onChange={this.onChangeExtrinsic}
-          onEnter={this.sendTx}
-        />
-        <Button.Group>
-          <TxButton
-            isBasic
-            isDisabled={!isValidUnsigned}
-            isUnsigned
-            label={t('Submit Unsigned')}
-            icon='sign-in'
-            extrinsic={extrinsic}
-          />
-          <Button.Or />
-          <TxButton
-            accountId={accountId}
-            isDisabled={!isValid}
-            isPrimary
-            label={t('Submit Transaction')}
-            icon='sign-in'
-            extrinsic={extrinsic}
-            ref={this.button}
-          />
-        </Button.Group>
-      </div>
-    );
-  }
+//   public render (): React.ReactNode {
+//     const { apiDefaultTxSudo, t } = this.props;
+//     const { isValid, isValidUnsigned, accountId } = this.state;
+//     const extrinsic = this.getExtrinsic() || apiDefaultTxSudo;
 
-  private nextState (newState: Partial<State>): void {
-    this.setState(
-      (prevState: State): State => {
-        const { method = prevState.method, accountId = prevState.accountId } = newState;
-        const isValid = !!(
-          accountId &&
-          accountId.length &&
-          method
-        );
+//     return (
+//       <div className='extrinsics--Selection'>
+//         <InputAddress
+//           label={t('using the selected account')}
+//           labelExtra={<BalanceFree label={<label>{t('free balance')}</label>} params={accountId} />}
+//           onChange={this.onChangeSender}
+//           type='account'
+//         />
+//         <Extrinsic
+//           defaultValue={apiDefaultTxSudo}
+//           label={t('submit the following extrinsic')}
+//           onChange={this.onChangeExtrinsic}
+//           onEnter={this.sendTx}
+//         />
+//         <Button.Group>
+//           <TxButton
+//             isBasic
+//             isDisabled={!isValidUnsigned}
+//             isUnsigned
+//             label={t('Submit Unsigned')}
+//             icon='sign-in'
+//             extrinsic={extrinsic}
+//           />
+//           <Button.Or />
+//           <TxButton
+//             accountId={accountId}
+//             isDisabled={!isValid}
+//             isPrimary
+//             label={t('Submit Transaction')}
+//             icon='sign-in'
+//             extrinsic={extrinsic}
+//             ref={this.button}
+//           />
+//         </Button.Group>
+//       </div>
+//     );
+//   }
 
-        return {
-          method,
-          isValid,
-          isValidUnsigned: !!method,
-          accountId
-        };
-      }
-    );
-  }
+//   private nextState (newState: Partial<State>): void {
+//     this.setState(
+//       (prevState: State): State => {
+//         const { method = prevState.method, accountId = prevState.accountId } = newState;
+//         const isValid = !!(
+//           accountId &&
+//           accountId.length &&
+//           method
+//         );
 
-  private onChangeExtrinsic = (method: Call | null = null): void => {
-    this.nextState({ method });
-  }
+//         return {
+//           method,
+//           isValid,
+//           isValidUnsigned: !!method,
+//           accountId
+//         };
+//       }
+//     );
+//   }
 
-  private onChangeSender = (accountId: string | null): void => {
-    this.nextState({ accountId });
-  }
+//   private onChangeExtrinsic = (method: Call | null = null): void => {
+//     this.nextState({ method });
+//   }
 
-  private getExtrinsic (): SubmittableExtrinsic | null {
-    const { api } = this.props;
-    const { method } = this.state;
+//   private onChangeSender = (accountId: string | null): void => {
+//     this.nextState({ accountId });
+//   }
 
-    if (!method) {
-      return null;
-    }
+//   private getExtrinsic (): SubmittableExtrinsic | null {
+//     const { api } = this.props;
+//     const { method } = this.state;
 
-    const fn = api.findCall(method.callIndex);
+//     if (!method) {
+//       return null;
+//     }
 
-    return api.tx[fn.section][fn.method](...method.args);
-  }
-}
+//     const fn = api.findCall(method.callIndex);
 
-export default withMulti(
-  Selection,
-  translate,
-  withApi
-);
+//     return api.tx[fn.section][fn.method](...method.args);
+//   }
+// }
+
+// export default withMulti(
+//   Selection,
+//   translate,
+//   withApi
+// );
